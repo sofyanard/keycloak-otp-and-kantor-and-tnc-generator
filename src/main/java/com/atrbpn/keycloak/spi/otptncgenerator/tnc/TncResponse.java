99bbb60e58c92d7ac8ebcb5b6517acfce219203a
Sofyan Ardianto <sofyanard@gmail.com>
2025-09-18 11:57:16 +0700

    update response from api :shrimp:

--- src/main/java/com/atrbpn/keycloak/spi/otptncgenerator/tnc/TncResponse.java
+++ src/main/java/com/atrbpn/keycloak/spi/otptncgenerator/tnc/TncResponse.java
@@ -1,27 +1,71 @@
 package com.atrbpn.keycloak.spi.otptncgenerator.tnc;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class TncResponse {
 
+    @JsonProperty("success")
     private boolean success;
-    private String message;
-    private String content;
-    private String url;
 
-    public TncResponse(boolean success, String message, String content, String url) {
-        this.success = success;
-        this.message = message;
-        this.content = content;
-        this.url = url;
-    }
+    @JsonProperty("message")
+    private String message;
+
+    @JsonProperty("data")
+    private TncResponseData tncResponseData;
 
     // Getters and Setters
+
+    public static class TncResponseData {
+
+        @JsonProperty("USERID")
+        private String userId;
+
+        @JsonProperty("USERNAME")
+        private String username;
+
+        @JsonProperty("REALM")
+        private String realm;
+
+        @JsonProperty("TNC")
+        private String tnc;
+
+        @JsonProperty("VERSI_TNC_USER")
+        private String versiTncUser;
+
+        @JsonProperty("TNC_TERBARU_ID")
+        private String tncTerbaruId;
+
+        @JsonProperty("VERSI_TNC_TERBARU")
+        private String versiTncTerbaru;
+
+        @JsonProperty("TANGGALTNC")
+        private String tanggaltnc;
+
+        @JsonProperty("KONTEN")
+        private String konten;
+
+        @JsonProperty("URL")
+        private String url;
+
+        @JsonProperty("STATUS_TNC")
+        private int statusTnc;
+
+        // Getters and Setters
+        public String getUserId() { return userId; }
+        public String getUsername() { return username; }
+        public String getRealm() { return realm; }
+        public String getTnc() { return tnc; }
+        public String getVersiTncUser() { return versiTncUser; }
+        public String getTncTerbaruId() { return tncTerbaruId; }
+        public String getVersiTncTerbaru() { return versiTncTerbaru; }
+        public String getTanggaltnc() { return tanggaltnc; }
+        public String getKonten() { return konten; }
+        public String getUrl() { return url; }
+        public int getStatusTnc() { return statusTnc; }
+    }
+
     public boolean isSuccess() { return success; }
-    public void setSuccess(boolean success) { this.success = success; }
     public String getMessage() { return message; }
-    public void setMessage(String message) { this.message = message; }
-    public String getContent() { return content; }
-    public void setContent(String content) { this.content = content; }
-    public String getUrl() { return url; }
-    public void setUrl(String url) { this.url = url; }
+    public TncResponseData getData() { return tncResponseData; }
 
 }
--- src/main/java/com/atrbpn/keycloak/spi/otptncgenerator/tnc/TncRestClient.java
+++ src/main/java/com/atrbpn/keycloak/spi/otptncgenerator/tnc/TncRestClient.java
@@ -15,7 +15,7 @@ import org.slf4j.LoggerFactory;
 public class TncRestClient {
 
     private static final Logger log = LoggerFactory.getLogger(TncRestClient.class);
-    
+    private static final ObjectMapper objectMapper = new ObjectMapper();
     public static String tncApiBaseUrl;
     
     static {
@@ -32,8 +32,6 @@ public class TncRestClient {
         }
     }
     
-    private static final ObjectMapper objectMapper = new ObjectMapper();
-
     public static TncResponse verifyUser(TncRequest request) throws IOException {
         
         // Do REST POST request
@@ -61,42 +59,7 @@ public class TncRestClient {
         conn.disconnect();
         String jsonString = jsonBuilder.toString();
 
-        // Read field "message" to determine type
-        String message = objectMapper.readTree(jsonString).get("message").asText();
-
-        // Return one of the class objects
-        Object responseObj;
-        if (message != null && message.toLowerCase().contains("pengguna sudah menggunakan t&c terbaru")) {
-            responseObj = objectMapper.readValue(jsonString, TncUpdatedResponse.class);
-        } else if (message != null && message.toLowerCase().contains("perbaharui t&c terbaru")) {
-            responseObj = objectMapper.readValue(jsonString, TncUnupdatedResponse.class);
-        } else {
-            throw new IOException("Unknown TNC response type: " + message);
-        }
-        return toCommonResponse(responseObj);
-    }
-
-    public static TncResponse toCommonResponse(Object responseObj) {
-        if (responseObj instanceof TncUnupdatedResponse) {
-            TncUnupdatedResponse r = (TncUnupdatedResponse) responseObj;
-            return new TncResponse(
-                r.isSuccess(),
-                r.getMessage(),
-                r.getData() != null ? r.getData().getKonten() : null,
-                r.getData() != null ? r.getData().getUrl() : null
-            );
-        } else if (responseObj instanceof TncUpdatedResponse) {
-            TncUpdatedResponse r = (TncUpdatedResponse) responseObj;
-            // Assume only one item in data list
-            TncUpdatedResponse.TncUpdatedResponseData d = (r.getData() != null && !r.getData().isEmpty()) ? r.getData().get(0) : null;
-            return new TncResponse(
-                r.isSuccess(),
-                r.getMessage(),
-                d != null ? d.getKontenText() : null,
-                d != null ? d.getUrl() : null
-            );
-        }
-        throw new IllegalArgumentException("Unknown TNC response type: " + responseObj.getClass());
+        return objectMapper.readValue(jsonString, TncResponse.class);
     }
 
 }
